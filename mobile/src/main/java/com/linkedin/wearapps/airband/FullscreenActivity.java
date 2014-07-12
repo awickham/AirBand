b6de8a9e034a822be43c6e43a58647546cbd2caa
Tony <tonyw@cs.utexas.edu>
2014-07-11 23:04:50 -0700

    Added layout structure for instrument options, with dummy drum set included

--- mobile/src/main/java/com/linkedin/wearapps/airband/FullscreenActivity.java
+++ mobile/src/main/java/com/linkedin/wearapps/airband/FullscreenActivity.java
@@ -3,9 +3,13 @@ package com.linkedin.wearapps.airband;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.linkedin.wearapps.airband.util.SystemUiHider;
@@ -13,9 +17,12 @@ import com.linkedin.wearapps.airband.util.SystemUiHider;
 
 /**
  * The main Activity, which is FullScreen unless the user explicitly taps the top of the screen to
- * show the status bar.
+ * show the status bar. This page displays instrument options for the user to choose from.
  */
 public class FullscreenActivity extends Activity {
+
+    private Typeface mWindSweptTf;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +33,27 @@ public class FullscreenActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen);
 
-        Typeface tf = Typeface.createFromAsset(getAssets(), "Windswept MF.ttf");
-        ((TextView) findViewById(R.id.title)).setTypeface(tf);
+        mWindSweptTf = Typeface.createFromAsset(getAssets(), "Windswept MF.ttf");
+        ((TextView) findViewById(R.id.title)).setTypeface(mWindSweptTf);
+        ((TextView) findViewById(R.id.choose_instrument)).setTypeface(mWindSweptTf);
 
-        final View anchorView = findViewById(R.id.anchor);
+        final LinearLayout anchorView = (LinearLayout) findViewById(R.id.anchor);
         SystemUiHider.getInstance(this, anchorView, 0).hide();
+
+        LinearLayout instrumentOptions = (LinearLayout) findViewById(R.id.instrument_options);
+        addInstrumentOption(R.drawable.drum_set2, R.string.drum_set, instrumentOptions,
+                R.color.yellow_transparent);
+    }
+
+    private void addInstrumentOption(int iconRes, int nameRes, ViewGroup instrumentOptionsGroup,
+                                     int colorRes) {
+        final LayoutInflater inflater = getLayoutInflater();
+        View option = inflater.inflate(R.layout.instrument_option, instrumentOptionsGroup, false);
+        ((ImageView) option.findViewById(R.id.instrument_icon)).setImageResource(iconRes);
+        final TextView instrumentName = (TextView) option.findViewById(R.id.instrument_name);
+        instrumentName.setText(nameRes);
+        instrumentName.setTypeface(mWindSweptTf);
+        option.setBackgroundColor(getResources().getColor(colorRes));
+        instrumentOptionsGroup.addView(option);
     }
 }
