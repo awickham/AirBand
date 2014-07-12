b29c72274935b6df71dc344e65ab2f2a47dba6b4
Andy Wickham <awickham@cs.utexas.edu>
2014-07-12 03:40:34 -0700

    Re-add ability to send message to phone.

--- wear/src/main/java/com/linkedin/wearapps/airband/MyActivity.java
+++ wear/src/main/java/com/linkedin/wearapps/airband/MyActivity.java
@@ -2,27 +2,34 @@ package com.linkedin.wearapps.airband;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.IntentSender;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
 import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Deque;
-import java.util.Iterator;
 import java.util.Queue;
 
-public class MyActivity  extends Activity implements SensorEventListener {
+public class MyActivity extends Activity implements SensorEventListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    /** Request code for launching the Intent to resolve Google Play services errors. */
+    private static final int REQUEST_RESOLVE_ERROR = 1000;
 
     private String TAG = "MyActivity";
 
@@ -34,6 +41,9 @@ public class MyActivity  extends Activity implements SensorEventListener {
     private Queue<Float> sensorQueue;
     private float vel;
     private boolean strum = true;
+
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mResolvingError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +69,25 @@ public class MyActivity  extends Activity implements SensorEventListener {
                 mFrameLayout.setBackgroundColor(Color.RED);
             }
         });
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -107,8 +136,49 @@ public class MyActivity  extends Activity implements SensorEventListener {
         }
     }
 
+    private void sendPlaySoundMessage() {
+        NodeApi.GetConnectedNodesResult nodes =
+                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+        for (Node node : nodes.getNodes()) {
+            Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(),
+                    Constants.PATH_PLAY_SOUND, new byte[0]);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Disconnected from Google Api Service");
+        }
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            mResolvingError = false;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Connected to Google Api Service");
+        }
+        mResolvingError = false;
+    }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
+    @Override
+    public void onConnectionSuspended(int i) {
     }
 }
